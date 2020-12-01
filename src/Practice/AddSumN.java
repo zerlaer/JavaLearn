d83d893126b062c0d429a9df266d7c57910d0882
zerlaer <1wwa100j>
2020-12-01 17:14:29 +0800

    添加ArrayList

--- src/Practice/AddSumN.java
+++ src/Practice/AddSumN.java
@@ -6,46 +6,43 @@ package Practice;
  * @name JavaLearn
  * @blog zerlaer.com
  */
-
-
 public class AddSumN {
-    public static void main(String[] args) {
-        System.out.println(addSum(10));
-        System.out.println(addSumOdd(10));
-        System.out.println(addSumEven(10));
+  public static void main(String[] args) {
+    System.out.println(addSum(10));
+    System.out.println(addSumOdd(10));
+    System.out.println(addSumEven(10));
+  }
+
+  // 计算1到n之和
+
+  public static int addSum(int n) {
+    int sum = 0;
+    for (int i = 0; i <= n; i++) {
+      sum += i;
     }
+    return sum;
+  }
+  // 计算1到n奇数之和
 
-    // 计算1到n之和
-
-    public static int addSum(int n) {
-        int sum = 0;
-        for (int i = 0; i <= n; i++) {
-            sum += i;
-        }
-        return sum;
+  public static int addSumOdd(int n) {
+    int sum = 0;
+    for (int i = 0; i <= n; i++) {
+      if (i % 2!=0) {
+        sum += i;
+      }
     }
-    // 计算1到n奇数之和
+    return sum;
+  }
 
-    public static int addSumOdd(int n) {
-        int sum = 0;
-        for (int i = 0; i <= n; i++) {
-            if (i % 2 != 0) {
-                sum += i;
-            }
-        }
-        return sum;
+  // 计算1到n偶数之和
+
+  public static int addSumEven(int n) {
+    int sum = 0;
+    for (int i = 0; i <= n; i++) {
+      if (i % 2==0) {
+        sum += i;
+      }
     }
-
-    // 计算1到n偶数之和
-
-    public static int addSumEven(int n) {
-        int sum = 0;
-        for (int i = 0; i <= n; i++) {
-            if (i % 2 == 0) {
-                sum += i;
-            }
-        }
-        return sum;
-    }
+    return sum;
+  }
 }
-
